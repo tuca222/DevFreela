f1679d85ba0bfc91f0bace374d3e1f21b3c63d7e
Tuca <tucaa222@gmail.com>
2023-03-27 16:11:20 -0300

    subindo estrutura do projeto

---
+++ src/DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,99 @@
+﻿using DevFreela.API.Models;
+using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+        public ProjectsController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        //api/projects GET
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            //Buscar todos ou filtrar
+            var projects = _projectService.GetAll(query);
+
+            return Ok(projects);
+        }
+
+        //api/projects/3 GetById
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var project = _projectService.GetById(id);
+
+            if (project == null)
+                return NotFound();
+            
+            return Ok();
+        }
+
+        //api/projects POST
+        [HttpPost]
+        public IActionResult Post([FromBody] NewProjectInputModel inputModel)
+        {
+            if (inputModel.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            var id = _projectService.Create(inputModel);
+
+            return CreatedAtAction(nameof(GetById), new {id = id}, inputModel);
+        }
+
+        //api/projects/2 PUT
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectInputModel inputModel)
+        {
+            if (inputModel.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+            _projectService.Update(inputModel);
+
+            return NoContent();
+        }
+
+        //api/projects/1
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _projectService.Delete(id);
+            return NoContent();
+        }
+
+        //api/projects/2/comments
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] NewCommentInputModel inputModel)
+        {
+            _projectService.CreateComment(inputModel);
+            return NoContent();
+        }
+
+        //api/projetcs/1/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            _projectService.Start(id);
+            return NoContent();
+        }
+
+        //api/projects/1/finish
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            _projectService.Finish(id);
+            return NoContent();
+        }
+    }
+}
---
+++ src/DevFreela.API/Program.cs
@@ -0,0 +1,38 @@
+using DevFreela.API.Models;
+using DevFreela.Application.Services.Implementations;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Infrastructure.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.Configure<OpeningTimeOption>(builder.Configuration.GetSection("OpeningTime"));
+
+builder.Services.AddSingleton<DevFreelaDbContext>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+
+
+builder.Services.AddScoped<ExempleClass>(e => new ExempleClass { Name = "Initial Stage"});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
